5635597e0bdd1494c886d7c32e7df8cedd2bc63f
JAM <1298946670@qq.com>
2022-03-08 18:29:11 +0800

    day1_runoob_instance_list

---
+++ runoob_instance/src/com/company/list/StringCompareEmp.java
@@ -0,0 +1,17 @@
+package com.company.list;
+
+public class StringCompareEmp {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String anotherString = "hello world";
+        Object objStr = str;
+
+        System.out.println(str.compareTo(anotherString));
+        System.out.println(str.compareToIgnoreCase(anotherString));
+        System.out.println(str.compareTo(objStr.toString()));
+    }
+}/* Output:
+-32
+0
+0
+*/
